9c01cd9fb1374b829e0f1379636bf8f0e383e6bc
aco97m <aleksandar.milojevic1997@gmail.com>
2020-04-27 16:00:13 +0200

    Proba

---
+++ Digitron/minidig.js
@@ -0,0 +1,24 @@
+
+function izracunaj(operacija) {
+    var broj1 = document.getElementById("br1").value;
+    var broj2 = document.getElementById("br2").value;
+    var rezultat = 0;
+
+
+    if ((isNaN(broj1) == false) && (isNaN(broj2) == false)) {
+        switch (operacija) {
+            case 1: rezultat = broj1 + broj2;
+                break;
+            case 2: rezultat = broj1 - broj2;
+                break;
+            case 3: rezultat = broj1 * broj2;
+                break;
+            case 4: rezultat = broj1 / broj2;
+                break;
+        }
+
+        document.getElementById("rez").value = rezultat;
+    } else {
+        alert("Digitron radi samo sa brojevima");
+    }
+}
