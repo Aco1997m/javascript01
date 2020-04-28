b4296b7c06a5e36723bfa91b895f02ca0d8e2c10
aco97m <aleksandar.milojevic1997@gmail.com>
2020-04-28 21:12:32 +0200

    no message

--- zadaca/alert1.js
+++ zadaca/alert1.js
@@ -3,9 +3,9 @@ function guess() {
     var guess = document.getElementById("number").value;
 
     if (guess == secret) {
-        alert("You've guessed it - congratulations! It's number "+secret+".");
+		document.getElementById('answer').innerHTML = "You've guessed it - congratulations! It's number "+secret+".";
     }
     else {
-        alert("Sorry, your guess is not correct... The secret number is not " + guess);
+        document.getElementById('answer').innerHTML = "Sorry, your guess is not correct... The secret number is not " + guess;
     }
 }
